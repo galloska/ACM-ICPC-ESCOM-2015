181a2cb222d7c319660ff7986891c077bd0a9674
galloska <skap19952008@hotmail.com>
2015-10-13 15:05:21 -0500

    Update J.cpp

--- J.cpp
+++ J.cpp
@@ -6,11 +6,15 @@ using namespace std;
 
 const int MAXN = 10005;
 
+set<int> up;
 lli pot[MAXN];
+set<int> down;
 int criba[MAXN];
+vector<lli> res;
 bool valid[MAXN];
 bool estado[MAXN];
 vector<lli> prime;
+bitset<MAXN> used;
 vector<int> lista[MAXN];
 
 void Pre(lli n){
@@ -38,27 +42,35 @@ void Pre(lli n){
     }
 }
 
+void Iniciar(int n){
+    pot[0] = 1;
+    for(int i=1;i<=n;i++)
+        pot[i] = ( pot[i-1] * (n+1) ) % MOD; 
+	
+	up.clear();
+    down.clear();
+    res.clear();
+
+    for(int i=1;i<=n;i++){
+        estado[i] = false;
+        down.insert(i);
+        valid[i] = true;
+    }	
+}
+
 int main(){
     Pre(10001);
 
 	int t,n,k,a;
+    int num,aux;
+    set<int> :: iterator it;
 
 	scanf("%d",&t);
 
 	for(int u=1;u<=t;u++){
 		scanf("%d %d",&n,&k);
 
-        pot[0] = 1;
-        for(int i=1;i<=n;i++)
-            pot[i] = ( pot[i-1] * (n+1) ) % MOD; 
-		
-		set<int> up,down;
-
-        for(int i=1;i<=n;i++){
-            estado[i] = false;
-            down.insert(i);
-            valid[i] = true;
-        }		
+        Iniciar(n);	
 
 		for(int i=0;i<k;i++){
 			scanf("%d",&a);
@@ -68,84 +80,59 @@ int main(){
 		}
 		
 		int turno = 0;
-		vector<lli> res;
 		
 		while(n){
 			if(!up.size() && !turno){
-                set<int> :: iterator it = down.end();
+                it = down.end();
 				it--;
 				int num = *it;
 				down.erase(num);
                 up.insert(num);
 			}
             else if(!up.size()){
-                set<int> :: iterator it = down.lower_bound(0);
+                it = down.lower_bound(0);
 				int num = *it;
 				down.erase(num);
                 up.insert(num);
 			}
 
-            bitset<10001> used;
+            used.reset();
 
 			if(!turno){
-				set<int> :: iterator it = up.end();
+				it = up.end();
 				it--;
-				int num = *it;
-                valid[num] = false;
-                up.erase(num);
-                int aux = num;
-
-                if(num!=1){
-                    for(int j=0;j<lista[num].size();j++){
-                        int a = lista[num][j];
-                        for(int h=0;h<lista[a].size();h++){
-                            int b = lista[a][h];
-                            if(!valid[b] || used[b]) continue;
-                            used[b] = true;
-                            if(estado[b]){
-                                up.erase(b);
-                                down.insert(b);
-                                estado[b] = false;
-                            }
-                            else{
-                                up.insert(b);
-                                down.erase(b);
-                                estado[b] = true;
-                            }
-                        }
-                    }
-                }
-				res.push_back(aux);
+				num = aux = *it;
 			}
 			else{
-				set<int> :: iterator it = up.lower_bound(0);
-				int num = *it;
-                valid[num] = false;
-                up.erase(num);
-                int aux = num;
+				it = up.lower_bound(0);
+				num = aux = *it;
+            }
 
-                if(num!=1){
-                    for(int j=0;j<lista[num].size();j++){
-                        int a = lista[num][j];
-                        for(int h=0;h<lista[a].size();h++){
-                            int b = lista[a][h];
-                            if(!valid[b] || used[b]) continue;
-                            used[b] = true;
-                            if(estado[b]){
-                                up.erase(b);
-                                down.insert(b);
-                                estado[b] = false;
-                            }
-                            else{
-                                up.insert(b);
-                                down.erase(b);
-                                estado[b] = true;
-                            }
+            valid[num] = false;
+            up.erase(num);
+
+            if(num!=1){
+                for(int j=0;j<lista[num].size();j++){
+                    int a = lista[num][j];
+                    for(int h=0;h<lista[a].size();h++){
+                        int b = lista[a][h];
+                        if(!valid[b] || used[b]) continue;
+                        used[b] = true;
+                        if(estado[b]){
+                            up.erase(b);
+                            down.insert(b);
+                            estado[b] = false;
+                        }
+                        else{
+                            up.insert(b);
+                            down.erase(b);
+                            estado[b] = true;
                         }
                     }
                 }
-				res.push_back(aux);
-			}
+            }
+			res.push_back(aux);
+
             turno = 1 - turno;
             n--;
 		}
